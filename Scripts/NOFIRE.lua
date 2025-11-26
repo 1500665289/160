180a6c1ee7ee1afeca3e78b0cda62a8b60f6fdb0
1500665289 <152504086+1500665289@users.noreply.github.com>
2025-11-26 16:51:44 +0800

    更新 NOFIRE.lua

--- Scripts/NOFIRE.lua
+++ Scripts/NOFIRE.lua
@@ -1,14 +1,17 @@
-﻿local NOFIRE = GameMain:GetMod("NOFIRE");
-local time = 0;
-local flag = 0;
+local NOFIRE = GameMain:GetMod("NOFIRE")
+
+function NOFIRE:OnInit()
+    self.time = 0
+    self.flag = 0
+    self:RegisterEvent("OnStep")
+end
 
 function NOFIRE:OnStep(dt)
-if flag == 0 then
-time = time + dt;
-if time >= 10  then
-flag = 1;
-CS.GameMain.Instance.NoBurning = true
+    if self.flag == 0 then
+        self.time = self.time + dt
+        if self.time >= 10 then
+            self.flag = 1
+            CS.GameMain.Instance.NoBurning = true
+        end
+    end
 end
-end
-end
-
